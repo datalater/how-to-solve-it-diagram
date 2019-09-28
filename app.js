073effba765a9f5278f617f5bc97b626fedf2352
datalater <the7mincheol@gmail.com>
2019-09-28 14:21:51 +0900

    Update how to solve it question text

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
 
 app.listen(3000, () => {
   console.log("Start! express server on port 3000");
 });
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
+});
+
+app.post("/export_pdf", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
--- public/main.js
+++ public/main.js
@@ -3,4 +3,5 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
 ctx.textAlign = "center";
-ctx.fillText("Hello World", canvas.width / 2, canvas.height / 2);
+var todo = `TODO\n1. PDF 공유`;
+ctx.fillText(todo, canvas.width / 2, canvas.height / 2);
