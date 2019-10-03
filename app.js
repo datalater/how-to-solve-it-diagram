9724898a8ee7cb9dcb25ab184b80821d59c4545c
datalater <the7mincheol@gmail.com>
2019-10-03 20:35:14 +0900

    Add result page using ejs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
   console.log("Start! express server on port 3000");
@@ -18,7 +19,20 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
 });
 
-app.post("/export_pdf", (req, res) => {
+app.post("/result", (req, res) => {
   console.log(req.body);
-  res.send(req.body);
+  res.render("result.ejs", {
+    answer1: req.body.answer1,
+    answer2: req.body.answer2,
+    answer3: req.body.answer3,
+    answer4: req.body.answer4,
+    answer5: req.body.answer5,
+    answer6: req.body.answer6,
+    answer7: req.body.answer7,
+    answer8: req.body.answer8,
+    answer9: req.body.answer9,
+    answer10: req.body.answer10,
+    answer11: req.body.answer11,
+    answer12: req.body.answer12
+  });
 });
