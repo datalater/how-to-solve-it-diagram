e4eb06bcbde64f9b1d5ac592a69d8c07f105191f
datalater <the7mincheol@gmail.com>
2019-10-03 10:48:50 +0900

    Add favicon

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
-const request = require("request");
-const port = process.env.PORT || 3000
+const favicon = require("serve-favicon");
+const path = require("path");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log("Start! express server on port 3000");
 });
-
-app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
