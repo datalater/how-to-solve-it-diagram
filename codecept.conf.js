9885ec00732c5dc44da02877cb3148d278b7c0e2
datalater <the7mincheol@gmail.com>
2019-11-16 22:19:38 +0900

    Ignore e2e test output

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,16 +1,16 @@
 exports.config = {
-  tests: './*.test.js',
-  output: './output',
+  tests: "./*.test.js",
+  output: "./output",
   helpers: {
     Puppeteer: {
-      url: 'http://localhost',
-      show: true
+      url: "http://localhost",
+      show: false
     }
   },
   include: {
-    I: './steps_file.js'
+    I: "./steps_file.js"
   },
   bootstrap: null,
   mocha: {},
-  name: 'how-to-solve-it-diagram'
-}
+  name: "how-to-solve-it-diagram"
+};
